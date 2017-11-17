bb8be27f281093caba3bc5ef86554cf46b3674f5
Alejandro Villarreal <avillarreal01@gmail.com>
2017-11-17 12:49:28 -0500

    fix(secrets): move to src for build

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import firebase from 'firebase'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './utils/registerServiceWorker'
-import { config } from '../secrets'
+import { config } from './secrets'
 
 firebase.initializeApp(config)
 
