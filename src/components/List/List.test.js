84d59a3eff2b4a97c7fbfa2a3f172a29916e9a7a
Alejandro Villarreal <avillarreal01@gmail.com>
2017-11-19 11:58:19 -0500

    fix(tests): add router around List test component

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -2,8 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import List from './List'
 
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import { store, history } from '../components.js'
+
 // eslint-disable-next-line
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<List />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <List />
+      </ConnectedRouter>
+    </Provider>,
+    div
+  )
 })
