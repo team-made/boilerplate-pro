c7cee14974eff3b63855bb32994992f26960b285
Alejandro Villarreal <avillarreal01@gmail.com>
2017-11-20 15:59:33 -0500

    feat(user): add persistent users

--- src/components/Builder/Builder.js
+++ src/components/Builder/Builder.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import firebase from 'firebase'
 import axios from 'axios'
-import {apiJSONFileCreator, indexHTMLFileCreator} from './FileGen.js'
+import { apiJSONFileCreator, indexHTMLFileCreator } from './FileGen.js'
 import { actions } from './index.js'
 import { store } from '../components.js'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const mapStateToProps = state => {
   return {
@@ -14,10 +14,12 @@ const mapStateToProps = state => {
 }
 const mapDispatchToProps = dispatch => {
   return {
-    handleRepoName: (event) => {
-      dispatch(actions.repoNameAction({
-        repoName: event.target.value
-      }))
+    handleRepoName: event => {
+      dispatch(
+        actions.repoNameAction({
+          repoName: event.target.value
+        })
+      )
     }
   }
 }
@@ -45,19 +47,19 @@ class Builder extends React.Component {
       has_projects: true,
       has_wiki: true
     }
-    await axios
-      .post(`https://api.github.com/user/repos`, data, config)
+    await axios.post(`https://api.github.com/user/repos`, data, config)
 
     await axios.put(
       `https://api.github.com/repos/${username}/${repoName}/contents/index.html`,
       indexHTMLFileCreator(),
       config
     )
-    await axios.put(
-      `https://api.github.com/repos/${username}/${repoName}/contents/api.json`,
-      apiJSONFileCreator(),
-      config
-    )
+    await axios
+      .put(
+        `https://api.github.com/repos/${username}/${repoName}/contents/api.json`,
+        apiJSONFileCreator(),
+        config
+      )
       .catch(err => console.error(err))
   }
 
@@ -73,11 +75,9 @@ class Builder extends React.Component {
     console.log('props', this.props)
     return (
       <div>
-        <div
-          className='field'
-          onSubmit={this.createRepo}
-          style={{ width: '400px', margin: '0 auto' }}
-        >
+        <div className='field' style={{ width: '400px', margin: '0 auto' }}>
+          <br />
+          <h1 className='title'>Builder</h1>
           <label className='label'>Repo Name</label>
           <div className='control'>
             <input
@@ -92,14 +92,18 @@ class Builder extends React.Component {
 
           <NavLink to={`/repos/${this.state.repoId}`}>
             <button className='button' onClick={this.createRepo}>
-            Create Repo
+              Create Repo
             </button>
           </NavLink>
         </div>
         {/* Eventually link to actual repo will go here */}
 
-        <button className='button' onClick={this.getCurrentUser}>Get Current user</button>
-        <button className='button' onClick={this.getUserRepo}>Show User Repos</button>
+        <button className='button' onClick={this.getCurrentUser}>
+          Get Current user
+        </button>
+        <button className='button' onClick={this.getUserRepo}>
+          Show User Repos
+        </button>
       </div>
     )
   }
