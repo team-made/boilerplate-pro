a17a812fc5f2938b765135d217e03ece6ef77670
Mitchell Stewart <mitchellwstewart@gmail.com>
2017-11-20 16:56:20 -0500

    fix(tests)

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import List from './List'
-
+import firebase from 'firebase'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import { store, history } from '../components.js'
 
+const config = {
+  apiKey:
+    process.env.FIREBASE_API_KEY || process.env.REACT_APP_FIREBASE_API_KEY,
+  messagingSenderId:
+    process.env.FIREBASE_MESSAGING_SENDER_ID ||
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: 'boilerplate-pro',
+  storageBucket: 'boilerplate-pro.appspot.com',
+  authDomain: 'boilerplate-pro.firebaseapp.com',
+  databaseURL: 'https://boilerplate-pro.firebaseio.com'
+}
+firebase.initializeApp(config)
 // eslint-disable-next-line
 it('renders without crashing', () => {
   const div = document.createElement('div')
--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom'
 import Navbar from './Navbar'
 import { Provider } from 'react-redux'
 import { store } from '../components.js'
-
+import firebase from 'firebase'
+const config = {
+  apiKey:
+    process.env.FIREBASE_API_KEY || process.env.REACT_APP_FIREBASE_API_KEY,
+  messagingSenderId:
+    process.env.FIREBASE_MESSAGING_SENDER_ID ||
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: 'boilerplate-pro',
+  storageBucket: 'boilerplate-pro.appspot.com',
+  authDomain: 'boilerplate-pro.firebaseapp.com',
+  databaseURL: 'https://boilerplate-pro.firebaseio.com'
+}
+firebase.initializeApp(config)
 // eslint-disable-next-line
 it('renders without crashing', () => {
   const div = document.createElement('div')
