eaa16835d0a6c39d6940f51ca7b16193ba3ea36c
Mitchell Stewart <mitchellwstewart@gmail.com>
2017-11-26 16:09:37 -0500

    fix(test):added router

--- src/components/TestIntegration/TestIntegration.test.js
+++ src/components/TestIntegration/TestIntegration.test.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import TestIntegration from './TestIntegration'
-
+import { ConnectedRouter } from 'connected-react-router'
 import { Provider } from 'react-redux'
-import { store } from '../components.js'
+import { store, history } from '../components.js'
 
 // eslint-disable-next-line
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(
     <Provider store={store}>
-      <TestIntegration />
+      <ConnectedRouter history={history}>
+        <TestIntegration />
+      </ConnectedRouter>
     </Provider>,
     div
   )
