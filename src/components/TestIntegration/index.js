bd10a92c154e4961c188bfa75dc3b7a428af1d3e
Alejandro Villarreal <avillarreal01@gmail.com>
2017-11-28 11:25:15 -0500

    tidy(tests): fix broken tests

--- src/components/TestIntegration/index.js
+++ src/components/TestIntegration/index.js
@@ -1,34 +1,5 @@
 // Include component
 import component from './TestIntegration.js'
 
-// Init reduxHelper
-import reduxHelper from '../../utils/reduxHelper.js'
-const reduxUtil = reduxHelper('TestIntegration')
-
-// Action Definitions
-const DUMMY_ACTION = reduxUtil.defineAction('DUMMY_ACTION')
-
-// Initial State
-const initialState = {
-  dummyState: false
-}
-
-// Make Actions
-const actions = {
-  dummyAction: reduxUtil.createAction(DUMMY_ACTION)
-}
-
-// Make reducer
-const reducer = reduxUtil.createReducer(
-  {
-    [DUMMY_ACTION]: (state, action) => {
-      let newState = { ...state, ...action.payload }
-      newState.dummyState = true
-      return newState
-    }
-  },
-  initialState
-)
-
 // Export
-export { component, actions, reducer }
+export { component }
