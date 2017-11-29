a8af4d653dfeef8f9ffdff39b49afc815edab5c4
Mitchell Stewart <mitchellwstewart@gmail.com>
2017-11-28 19:14:38 -0500

    progress bar

--- src/components/QuickBuilder/QuickBuilder.js
+++ src/components/QuickBuilder/QuickBuilder.js
@@ -24,20 +24,22 @@ class QuickBuilder extends Component {
       working: false,
       progress: '',
       content: '',
+      status: '',
       placeholder: `Repo Name (ex. '${this.props.currentRepo.name}')`
     }
     this.startCloner = this.startCloner.bind(this)
   }
 
   startCloner (e) {
+    console.log('owner', this.props.currentRepo)
     e.preventDefault()
-    console.log('input', e.target.input.value)
     this.setState({ working: true })
     this.setState({ content: `sending request to server` })
     const githubToken = this.props.user.githubToken
     const githubUsername = this.props.user.githubUsername
-    const name = this.props.currentRepo.name
-    const owner = this.props.currentRepo.owner.login
+    const {name, owner} = this.props.currentRepo
+    // const owner = this.props.currentRepo.owner.login
+
     this.props.user.uid &&
     firebase
       .firestore()
@@ -45,16 +47,18 @@ class QuickBuilder extends Component {
       .doc(this.props.user.uid)
       .collection('repos')
       .doc(e.target.input.value)
-      .onSnapshot(doc =>
+      .onSnapshot(doc => {
         console.log('USER SNAPSHOT', doc.exists && doc.data())
-      )
+        this.setState({content: doc.exists && doc.data().status})
+      })
+
     axios
       .post('https://boilerplate-pro-server.herokuapp.com/github/hyperClone', {
         repoName: e.target.input.value,
         githubUsername: githubUsername,
         githubToken: githubToken,
         name: name,
-        owner: owner
+        owner: owner.login
       })
       .then(result => {
         if (result.status === 200) {
@@ -67,7 +71,7 @@ class QuickBuilder extends Component {
   }
 
   componentDidMount () {
-    console.log(this.props.repoName)
+    console.log('props', this.props)
   }
 
   render () {
@@ -80,7 +84,6 @@ class QuickBuilder extends Component {
                 className='input'
                 type='text'
                 name='input'
-                // defaultValue={this.props.currentRepo.name}
                 placeholder={this.state.placeholder}
               />
             </div>
@@ -97,7 +100,7 @@ class QuickBuilder extends Component {
           <div style={{ border: 'solid 1px black', padding: '10px' }}>
             <span style={{ fontWeight: 800 }}>{this.state.content}</span>
             <br />
-            {this.state.progress}
+            {this.state.content}
             {this.state.warningText && (
               <p className='help'>{this.state.warningText}</p>
             )}
